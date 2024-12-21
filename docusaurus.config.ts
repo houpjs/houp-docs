9220a2b3a1e375e25fa6358d952f539fcea6eb23
fpsqdb <fpsqdb@gmail.com>
2024-12-21 11:12:27 +0800

    feat: change baseURl to "/"

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   url: 'https://houp-docs.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/houp-docs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
